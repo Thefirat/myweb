f3b93a274ad6efe6f54aadb2e629f501a7fab008
Lorena Echeverri <lorena.echeverri@medieinstitutet.se>
2022-06-08 14:30:12 +0200

    Database conn

---
+++ classes/Database.php
@@ -0,0 +1,19 @@
+<?php
+
+class Database
+{
+    private $host = "localhost";
+    private $user = "root";
+    private $password = "";
+    private $db = "myweb-db";
+
+    protected $conn;
+    public function __construct()
+    {
+        $this->conn = new mysqli($this->host, $this->user, $this->password, $this->db);
+
+        if (!$this->conn){
+            die("connection faild");
+        }
+    }
+}
